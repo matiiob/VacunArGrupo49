7ee350a2147d207396b48d1851ce825857e2f857
AlexAnders2023 <m.alejandro.q@gmail.com>
2023-10-03 18:22:07 -0300

    clase cita vacuna
    desarrollo de clase citavacuna atributos getter setter

---
+++ src/Entidades/citaVacuna.java
@@ -0,0 +1,97 @@
+
+package Entidades;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @AlexAnders
+ */
+public class citaVacuna {
+    private int codCita;
+    private Ciudadano persona;
+    private int codRefuerzo;
+    private String fechaHoraCita;
+    private String centroVacunacion;
+    private LocalDate fechaHoraColoca; 
+    private int dosis;
+
+    public citaVacuna() {
+    }
+
+    public citaVacuna(Ciudadano persona, int codRefuerzo, String fechaHoraCita, String centroVacunacion, LocalDate fechaHoraColoca, int dosis) {
+        this.persona = persona;
+        this.codRefuerzo = codRefuerzo;
+        this.fechaHoraCita = fechaHoraCita;
+        this.centroVacunacion = centroVacunacion;
+        this.fechaHoraColoca = fechaHoraColoca;
+        this.dosis = dosis;
+    }
+
+    public citaVacuna(int codCita, Ciudadano persona, int codRefuerzo, String fechaHoraCita, String centroVacunacion, LocalDate fechaHoraColoca, int dosis) {
+        this.codCita = codCita;
+        this.persona = persona;
+        this.codRefuerzo = codRefuerzo;
+        this.fechaHoraCita = fechaHoraCita;
+        this.centroVacunacion = centroVacunacion;
+        this.fechaHoraColoca = fechaHoraColoca;
+        this.dosis = dosis;
+    }
+
+    public int getCodCita() {
+        return codCita;
+    }
+
+    public void setCodCita(int codCita) {
+        this.codCita = codCita;
+    }
+
+    public Ciudadano getPersona() {
+        return persona;
+    }
+
+    public void setPersona(Ciudadano persona) {
+        this.persona = persona;
+    }
+
+    public int getCodRefuerzo() {
+        return codRefuerzo;
+    }
+
+    public void setCodRefuerzo(int codRefuerzo) {
+        this.codRefuerzo = codRefuerzo;
+    }
+
+    public String getFechaHoraCita() {
+        return fechaHoraCita;
+    }
+
+    public void setFechaHoraCita(String fechaHoraCita) {
+        this.fechaHoraCita = fechaHoraCita;
+    }
+
+    public String getCentroVacunacion() {
+        return centroVacunacion;
+    }
+
+    public void setCentroVacunacion(String centroVacunacion) {
+        this.centroVacunacion = centroVacunacion;
+    }
+
+    public LocalDate getFechaHoraColoca() {
+        return fechaHoraColoca;
+    }
+
+    public void setFechaHoraColoca(LocalDate fechaHoraColoca) {
+        this.fechaHoraColoca = fechaHoraColoca;
+    }
+
+    public int getDosis() {
+        return dosis;
+    }
+
+    public void setDosis(int dosis) {
+        this.dosis = dosis;
+    }
+    
+}
