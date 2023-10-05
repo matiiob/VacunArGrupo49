a6a94cdd720b8d17ba741b30d47d61ff2738d022
matiiob <141784145+matiiob@users.noreply.github.com>
2023-10-05 19:33:32 -0300

    main con pruebas

--- src/vacunargrupo49/VacunArGrupo49.java
+++ src/vacunargrupo49/VacunArGrupo49.java
@@ -1,41 +1,67 @@
-
 package vacunargrupo49;
 
 import AccesoADatos.CiudadanoData;
 import AccesoADatos.LaboratorioData;
 import Entidades.Ciudadano;
 import Entidades.Laboratorio;
-import Entidades.Vacuna;
-import java.time.LocalDate;
-import java.time.Month;
-
-
 
 public class VacunArGrupo49 {
 
-
     public static void main(String[] args) {
-          
-        // CODIGO DE PRUEBAS
-        
-//        Ciudadano juan = new Ciudadano(123, "Juan Lopez", "juan@lopez", "012", null, "Salud");
+
+//        CODIGO DE PRUEBAS
+
+//      Prueba Guardar Ciudadano
+//        Ciudadano carla = new Ciudadano(456, "Carla Gomez", "CarlaG123@hotmail.com", "15265948", "Gripe", "Seguridad");
 //        CiudadanoData ciu = new CiudadanoData();
-//        ciu.guardarCiudadano(juan);
-//            Laboratorio labo = new Laboratorio(1, 33445566779L, "Pfizer", "USA", "Complejo LUMINA Thames\n" +
-//                                                "Colectora Panamericana 1804, Piso 1° - Sector B - Lado Sur\n" +
-//                                                "Villa Adelina, Provincia de Buenos Aires - CP B1607EEV", true);
-//            LaboratorioData laboData = new LaboratorioData();
-//            laboData.modificarLaboratorio(labo);
-  
-    
-    
-    
-    
-    
+//        ciu.guardarCiudadano(carla);
+
+//      Prueba metodo modificar ciudadano
+//        CiudadanoData ciu = new CiudadanoData();
+//        Ciudadano carla = new Ciudadano(2,456, "Carla Gomez", "CarlaG123@hotmail.com", "15265948", "Resfriado", "Seguridad");
+//        ciu.modificarCiudadano(carla);
+
+//      Prueba metodo Buscar ciudadano por DNI
+//        CiudadanoData ciu = new CiudadanoData();
+//        System.out.println(ciu.buscarCiudadanoDni(456));
+
+//      Prueba metodo Buscar ciudadano por ID
+//        CiudadanoData ciu = new CiudadanoData();
+//        System.out.println(ciu.buscarCiudadanoId(1));
+
+        
+//      Prueba Modificar LAboratorio
+//        Laboratorio labo = new Laboratorio(2, 33445566779L, "Pfizer", "USA", "Colectora Panamericana", true);
+//        LaboratorioData laboData = new LaboratorioData();
+//        laboData.modificarLaboratorio(labo);
+
+//      Prueba Eliminar Laboratorio
+//        Laboratorio lab = new Laboratorio(1,33556644339L, "Bayer", "Argentina", "CABA, Calle numero", true);
+//        LaboratorioData labData = new LaboratorioData();
+//        labData.eliminarLaboratorio(lab.getIdLaboratorio());
+
+//      Prueba Buscar Laboratorio por ID
+//        LaboratorioData labData = new LaboratorioData();
+//        Laboratorio labo = labData.buscarLaboratorioId(1, true);
+//        if (labo != null) {
+//            System.out.println(labo.toString());
+//        } else {
+//            System.out.println("El laboratorio buscado no existe.");
+//        }
+        
+//      Prueba Buscar Laboratorio por CUIT
+//        LaboratorioData labData = new LaboratorioData();
+//        Laboratorio labo = labData.buscarLaboratorioCuit(33556644339L, true);
+//        if (labo != null) {
+//            System.out.println(labo.toString());
+//        } else {
+//            System.out.println("El laboratorio buscado no existe.");
+//        }
+//      PRUEBA LISTAR LABORATORIOS
+//        LaboratorioData labData = new LaboratorioData();
+//        for (Laboratorio labo : labData.listarLaboratorios(true)) {
+//            System.out.println(labo);
+//        }
+        
     }
-
-
-
-    }
-
-
+}
