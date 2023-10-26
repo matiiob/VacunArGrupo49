1b33acbb1ce2d3c5a7a972363f81d5a39b5f51dc
GonzaloDelCastello <141686474+GonzaloDelCastello@users.noreply.github.com>
2023-10-26 18:43:19 -0300

    GestionarVacunas

--- src/AccesoADatos/Conexion.java
+++ src/AccesoADatos/Conexion.java
@@ -22,7 +22,7 @@ public class Conexion {
                 Class.forName("org.mariadb.jdbc.Driver");
                 connection = DriverManager.getConnection(URL + DB, USUARIO, PASSWORD);
 
-                JOptionPane.showMessageDialog(null, "Conexion Exitosa"); // (es solo para probar)
+//                JOptionPane.showMessageDialog(null, "Conexion Exitosa"); // (es solo para probar)
             } catch (ClassNotFoundException ex) {
                 JOptionPane.showMessageDialog(null, "Error al cargar el driver." + ex.getMessage());
 
