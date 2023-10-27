f0d65f089bce61338c42cb3e2795dba306fc20c2
matiiob <141784145+matiiob@users.noreply.github.com>
2023-10-27 19:59:29 -0300

    Main
    Ahora se puede ejecutar el programa directamente apretando el boton RUN PROJECT o con F6

--- src/vacunargrupo49/VacunArGrupo49.java
+++ src/vacunargrupo49/VacunArGrupo49.java
@@ -1,18 +1,10 @@
 package vacunargrupo49;
 
-import AccesoADatos.CiudadanoData;
-import AccesoADatos.LaboratorioData;
-import AccesoADatos.VacunaData;
-import Entidades.Ciudadano;
-import Entidades.Laboratorio;
-import Entidades.Vacuna;
-import java.time.LocalDate;
-import java.time.Month;
+import Vistas.Principal;
 
 public class VacunArGrupo49 {
 
     public static void main(String[] args) {
-
-
+        Principal.main(args);
     }
 }
